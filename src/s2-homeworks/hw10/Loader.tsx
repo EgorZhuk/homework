7060568650133a34a262a5adfe73a91288a1fc41
EgorZhuk <egorbug@gmail.com>
2023-02-19 22:48:45 +0300

    HW10 is done

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,23 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+  <div className={s.container}>
+    <svg className={s.loader} viewBox="0 0 50 50">
+      <defs>
+        <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+          <stop offset="0%" stopColor="#fff" />
+          <stop offset="100%" stopColor="#512DE4" />
+        </linearGradient>
+      </defs>
+      <circle
+              cx="25"
+              cy="25"
+              r="20"
+              fill='none'
+              strokeWidth="5"
+              strokeLinecap='round'
+              stroke='url(#gradient)'
+      ></circle>
+    </svg>
+  </div>
+)
