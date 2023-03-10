1c517d6875166cf7465332082b97bfb081b55c42
EgorZhuk <egorbug@gmail.com>
2023-03-10 12:52:45 +0300

    HW14 is done

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -3,7 +3,7 @@ import s2 from '../../s1-main/App.module.css'
 import s from './HW14.module.css'
 import axios from 'axios'
 import SuperDebouncedInput from './common/c8-SuperDebouncedInput/SuperDebouncedInput'
-import {useSearchParams} from 'react-router-dom'
+import {createSearchParams, useSearchParams} from 'react-router-dom';
 
 /*
 * 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
@@ -34,22 +34,20 @@ const HW14 = () => {
         setLoading(true)
         getTechs(value)
             .then((res) => {
-                // делает студент
-
-                // сохранить пришедшие данные
-
-                //
+                if (res) {
+                    setTechs(res.data.techs)
+                }
+                setLoading(false)
             })
     }
 
     const onChangeText = (value: string) => {
         setFind(value)
-        // делает студент
-
-        // добавить/заменить значение в квери урла
-        // setSearchParams(
-
-        //
+        const val = createSearchParams({find: value})
+        setSearchParams(val)
+        // const findQuery: {find?: string} = value ? {find: value} : {}
+        // const params = Object.fromEntries(searchParams)
+        // setSearchParams({...params, ...findQuery})
     }
 
     useEffect(() => {
